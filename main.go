06f7b4b022dad180860d0abebea50a690f045bfe
Lukas Bischof <me@luk4s.dev>
2022-04-16 19:02:28 +0200

    Update server configs

--- main.go
+++ main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/template/pug"
 	"github.com/qinains/fastergoding"
 	"log"
+	"os"
 )
 
 func main() {
-	fastergoding.Run()
+	if os.Getenv("APP_ENV") == "development" {
+		fastergoding.Run()
+		fmt.Println("Starting with Fastergoding")
+	}
+
 	engine := pug.New("./views", ".pug")
 	app := fiber.New(fiber.Config{
-		Views:       engine,
-		AppName:     "luk4s.dev",
-		ViewsLayout: "layouts/application",
+		Views:        engine,
+		AppName:      "luk4s.dev",
+		ViewsLayout:  "layouts/application",
+		ServerHeader: "Luk4serve",
+		ETag:         true,
 	})
 
+	app.Use(compress.New(compress.Config{
+		Level: compress.LevelBestSpeed,
+	}))
+
 	app.Get("/", func(ctx *fiber.Ctx) error {
+		ctx.Accepts("html", "text/html")
 		return ctx.Render("index", fiber.Map{})
 	})
 
