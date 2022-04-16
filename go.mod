65c49789643a56df89d98fede954b9ffde5414bb
Lukas Bischof <me@luk4s.dev>
2022-04-16 21:36:50 +0200

    Add a very cool visitors counter

--- go.mod
+++ go.mod
@@ -3,15 +3,19 @@ module github.com/lukasbischof/luk4s.dev
 go 1.18
 
 require (
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/gofiber/fiber/v2 v2.30.0
 	github.com/gofiber/template v1.6.26
 	github.com/qinains/fastergoding v0.0.2
+	golang.org/x/text v0.3.7
 )
 
 require (
 	github.com/Joker/hpp v1.0.0 // indirect
 	github.com/Joker/jade v1.1.3 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/template/pug"
 	"github.com/qinains/fastergoding"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 	"log"
 	"os"
+	"strconv"
 )
+
+var ctx = context.Background()
 
 func main() {
 	if os.Getenv("APP_ENV") == "development" {
@@ -16,22 +23,20 @@ func main() {
 		fmt.Println("Starting with Fastergoding")
 	}
 
-	engine := pug.New("./views", ".pug")
-	app := fiber.New(fiber.Config{
-		Views:        engine,
-		AppName:      "luk4s.dev",
-		ViewsLayout:  "layouts/application",
-		ServerHeader: "Luk4serve",
-		ETag:         true,
+	redisDb, err := strconv.ParseInt(getEnv("REDIS_DB", "0"), 10, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     getEnv("REDIS_ADDR", "localhost:6379"),
+		Password: getEnv("REDIS_PASSWORD", ""),
+		DB:       int(redisDb),
 	})
 
-	app.Use(compress.New(compress.Config{
-		Level: compress.LevelBestSpeed,
-	}))
+	app := boot()
 
-	app.Get("/", func(ctx *fiber.Ctx) error {
-		ctx.Accepts("html", "text/html")
-		return ctx.Render("index", fiber.Map{})
+	app.Get("/", func(c *fiber.Ctx) error {
+		return renderIndex(c, rdb)
 	})
 
 	app.Static("/public", "./public", fiber.Static{
@@ -46,4 +51,43 @@ func main() {
 	})
 
 	log.Fatal(app.Listen(":3000"))
+}
+
+func renderIndex(c *fiber.Ctx, rdb *redis.Client) error {
+	c.Accepts("html", "text/html")
+
+	visitorCount, err := IncreaseVisitorCount(rdb)
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+	}
+
+	p := message.NewPrinter(language.English)
+	return c.Render("index", fiber.Map{
+		"visitorCount": p.Sprintf("%d", visitorCount),
+	})
+}
+
+func boot() *fiber.App {
+	engine := pug.New("./views", ".pug")
+	app := fiber.New(fiber.Config{
+		Views:        engine,
+		AppName:      "luk4s.dev",
+		ViewsLayout:  "layouts/application",
+		ServerHeader: "Luk4serve",
+		ETag:         true,
+	})
+
+	app.Use(compress.New(compress.Config{
+		Level: compress.LevelBestSpeed,
+	}))
+
+	return app
+}
+
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
 }
